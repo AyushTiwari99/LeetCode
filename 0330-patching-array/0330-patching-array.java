6362260e9281f47797042d9c9a58982e13f34213
Ayush Tiwari <80597146+AyushTiwari99@users.noreply.github.com>
2023-03-20 23:52:48 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (38.49%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minPatches(int[] nums, int n) 
+    {
+        long missing =1;
+        int count=0,i=0;
+        while(missing<=n)
+        {
+            if(i<nums.length && missing>=nums[i])
+            {
+                missing+=nums[i];
+                i++;
+            }
+            else
+            {
+                count++;
+                missing+=missing;
+            }
+        }
+        return count;
+    }
+}
