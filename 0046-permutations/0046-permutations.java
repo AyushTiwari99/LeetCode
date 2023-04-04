b13ef076d38101e4f25337094899ff1dc151a16b
Ayush Tiwari <80597146+AyushTiwari99@users.noreply.github.com>
2023-04-04 09:32:35 +0530

    Time: 1 ms (95.22%), Space: 42.8 MB (37.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) 
+    {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> l1=new ArrayList<>();
+        boolean arr[]=new boolean[nums.length];
+        compute(res,l1,arr,nums);
+        return res;
+    }
+    public static void compute(List<List<Integer>> res,List<Integer> l1 , boolean fre[], int nums[])
+    {
+        if(l1.size()==nums.length) 
+        {
+            res.add(new ArrayList<>(l1));
+            return;
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!fre[i])
+            {
+                l1.add(nums[i]);
+                fre[i]=true;
+                compute(res,l1,fre,nums);
+                l1.remove(l1.size()-1);
+                fre[i]=false;
+            }  
+        }
+    }
+}
